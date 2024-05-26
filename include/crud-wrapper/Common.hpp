1d4abbace441ddeb1d96f0dfb16bf428fde4213c
Ahmed Elanani <AhmedSElanani@gmail.com>
2024-05-26 10:08:09 +0200

    Add CrudWrapper construction tests
    - Added project root directory definition from CMake to the common
      header, so that it can be used later on in unit tests
    - Added a test for negative scenario, where the database path doesn't
      exist
    - Added tests for positive scenario, where the paths of the database
      files exist

---
+++ include/crud-wrapper/Common.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+
+/// @brief namespace for common
+namespace sql_with_cpp::common {
+
+const std::string kprojectRootPath{PROJECT_ROOT_PATH};
+
+} // namespace sql_with_cpp::common
--- test/CrudWrapper_test.cpp
+++ test/CrudWrapper_test.cpp
@@ -1,3 +1,5 @@
+#include "crud-wrapper/Common.hpp"
+
 #include "crud-wrapper/CrudWrapper.hpp"
 
 #include "gtest/gtest.h"
@@ -7,6 +9,14 @@ namespace sql_with_cpp_test::crudWrapper_test {
 using namespace ::sql_with_cpp;
 
 TEST(TestingConstruction, ConstructingCrudWrapper) {
+  EXPECT_THROW(
+      { CrudWrapper{"/non/existing/path"}; },
+      std::filesystem::filesystem_error);
+
+  EXPECT_NO_THROW({ CrudWrapper{common::kprojectRootPath + "/db/album.db"}; });
+  EXPECT_NO_THROW(
+      { CrudWrapper{common::kprojectRootPath + "/db/scratch.db"}; });
+  EXPECT_NO_THROW({ CrudWrapper{common::kprojectRootPath + "/db/world.db"}; });
 }
 
 } // namespace sql_with_cpp_test::crudWrapper_test
